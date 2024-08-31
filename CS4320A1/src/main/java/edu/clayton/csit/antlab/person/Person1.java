dc3ee447e7137add3363f1640954d749cb7c053a
Birame Diop <79890569+bidiop@users.noreply.github.com>
2024-08-31 13:39:52 -0400

    Update Birame_Person1.java

--- CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
+++ CS4320A1/src/main/java/edu/clayton/csit/antlab/person/Person1.java
@@ -1,5 +1,6 @@
 package edu.clayton.csit.antlab.person;
 
+Import java. util.Scanner;
 /**
  *  A simple class for person 1
  *  returns their name and a
@@ -16,34 +17,55 @@ public class Person1 {
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person1(String pname) {
-    name = pname;
-  }
-  	/**
-	 * This method should take the string
-	 * input and return its characters rotated
-	 * 2 positions.
-	 * given "gtg123b" it should return
-	 * "g123bgt".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
-	}
-	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+      public Person1(String pname) {
+        name = pname;
+    }
 
+    /**
+     * This method takes the input string and returns its characters rotated by 2 positions.
+     * For example, given "gtg123b" it should return "g123bgt".
+     * 
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+        // Check if the input is not null and its length is greater than 2
+        if (input != null && input.length() > 2) {
+            // Rotate the string by 2 positions
+            return input.substring(2) + input.substring(0, 2);
+        }
+        // If the input is too short to rotate, return it as is
+        return input;
+    }
+
+    /**
+     * Return a string representation of this object that varies with an input string
+     * @param input the varying string
+     * @return the string representing the object
+     */
+    public String toString(String input) {
+        return name + ": " + calc(input);
+    }
+
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter their name
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Create a Person1 object with the user's name
+        Person1 person = new Person1(name);
+
+        // Prompt the user to enter a string to modify
+        System.out.print("Enter a string to rotate by 2 positions: ");
+        String input = scanner.nextLine();
+
+        // Print the result
+        System.out.println(person.toString(input));
+
+        // Close the scanner
+        scanner.close();
+    }
 }
